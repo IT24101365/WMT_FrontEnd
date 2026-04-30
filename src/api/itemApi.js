547f7fd40b30b076257c80a25279991b9b84cfa1
IT24101365 <IT24101365@my.sliit.lk>
2026-04-30 14:52:05 +0530

    Updated

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://wmtbackend-production-bf38.up.railway.app/",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://wmtbackend-production-bf38.up.railway.app/api",
 });
 
 export const getItems = () => API.get("/items");
